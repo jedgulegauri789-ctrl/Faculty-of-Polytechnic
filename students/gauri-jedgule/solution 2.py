304c68ff22bba03495d8df5666e1401a9b52bf4e
jedgulegauri789-ctrl <jedgulegauri789@gmail.com>
2026-06-11 11:35:29 +0530

    Update solution 2.py

--- students/gauri-jedgule/solution 2.py
+++ students/gauri-jedgule/solution 2.py
@@ -5,3 +5,15 @@ def greet():
     age=int(input("Enter your age:"))
     print("Hi",name+"!Next year you will be", age+1)
 greet()
+
+
+#problem 2
+#while numbers
+num=10
+while True:
+    uinp=int(input("1,2,3,4,5,6,7,8,9,10"))
+    if uinp!=num :
+         print("incorrect")
+    else:
+         print("correct")
+         break
